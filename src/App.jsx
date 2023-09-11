3662ff50077ba209575181299c7175205ed89fcb
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-11 13:48:13 -0300

    atividade3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { StudentCard } from "./components/StudentCard";
+
 function App() {
    return (
       <>
-
+         <StudentCard name="Alex" age={33} country="Brasil" />
+         <StudentCard name="Otavio" age={25} country="Brasil" />
+         <StudentCard name="Lara" age={28} country="Portugal" />
       </>
    );
 }
