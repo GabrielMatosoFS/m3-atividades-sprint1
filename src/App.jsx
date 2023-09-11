841ebc9a59ef0419f07001ad22a446a26af7e905
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-11 14:03:27 -0300

    atividade4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { DefaultTemplate } from "./components/DefaultTemplate";
+
 function App() {
    return (
       <>
-
+         <DefaultTemplate>
+            <h1>Oi, eu sou um elemento de teste</h1>
+         </DefaultTemplate>
       </>
    );
 }
