dfbb2cd82c78f5c2c546558d64a336da8d994cf6
Gabriel Matoso <gabriel_wsm@hotmail.com>
2023-11-26 16:32:25 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 function App() {
-   return (
-      <>
+  const person = {
+    name: "Gabriel",
+    module: "M3",
+    age: 28,
+  };
 
-      </>
-   );
+  const alertName = () => {
+    alert(person.name);
+  }
+
+  return (
+    <>
+      <li>
+        <h3>{person.name} - {person.module}</h3>
+        <p>Tenho: {person.age} anos</p>
+        <button onClick={alertName}>Quem sou eu?</button>
+      </li>
+    </>
+  )
 }
 
-export default App;
+export default App
